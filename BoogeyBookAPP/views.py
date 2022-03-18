be5c94c6d45c50a5be2bfab31589f9d3c8955307
Pau <paullobet99@gmail.com>
2022-03-19 00:02:39 +0100

    Templates divided (Home and Autentication)

--- BoogeyBookAPP/views.py
+++ BoogeyBookAPP/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def autentication(request):
-    return render(request,"homeTemplate.html")
+    return render(request, "autTemplate.html")
+
+
+def home(request):
+    return render(request, "homeTemplate.html")
