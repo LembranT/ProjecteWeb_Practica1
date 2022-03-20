25a99841abbb838fee818409569bcd7206179c81
Pau <paullobet99@gmail.com>
2022-03-20 11:10:31 +0100

    Form ended

--- BoogeyBookAPP/views.py
+++ BoogeyBookAPP/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from BoogeyBookAPP.models import Book
 
 # Create your views here.
@@ -7,6 +7,7 @@ from BoogeyBookAPP.models import Book
 from django.views.generic import View
 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 class VRegister(View):
@@ -15,24 +16,26 @@ class VRegister(View):
         return render(request, "autTemplate.html", {"form": form})
 
     def post(self, request):
-        pass
+        form = UserCreationForm(request.POST)
+        user = form.save()
+        login(request, user)
+        return redirect('Home')
 
 
 def home(request):
     return render(request, "homeTemplate.html")
 
+
 def search(request):
-
     if request.GET["data"]:
-        #message="Book searched: %r" %request.GET["data"]
-        book=request.GET["data"]
+        # message="Book searched: %r" %request.GET["data"]
+        book = request.GET["data"]
 
-        books=Book.objects.filter(name__icontains=book)
+        books = Book.objects.filter(name__icontains=book)
 
-        return render(request, "results.html", {"books":books, "query":book})
+        return render(request, "results.html", {"books": books, "query": book})
 
     else:
-        message="You just entered nothing."
-
+        message = "You just entered nothing."
 
     return HttpResponse(message)
