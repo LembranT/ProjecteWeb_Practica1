37c38197d5e43f9d4600d5ced4a2c8563f9e38ed
Víctor <lembrant32@gmail.com>
2022-03-20 18:24:59 +0100

    BDD amb 2 llibres

--- BoogeyBookAPP/models.py
+++ BoogeyBookAPP/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-
-# Create your models here.
 class Reader(models.Model):
     reader_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=20)
